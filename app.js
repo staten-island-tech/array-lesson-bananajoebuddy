8f7660ef0bd2805e546224179a4b3c2af961fc57
bananajoebuddy <144480594+bananajoebuddy@users.noreply.github.com>
2024-10-07 12:19:44 -0400

    sdfagasfafsf

---
+++ app.js
@@ -0,0 +1,29 @@
+/* const students = ["Matt", "Mark", "Josephine", "Thomas"]; */
+/* console.log(student[0]); //gets matt
+students.forEach((student) => console.log(student)); //allows us to loop through every element of the list */
+
+/* const movie = [
+  {
+    title: "Star Wars",
+    release: 1977,
+    actors: ["mark", "harrison"],
+  },
+  {
+    title: "Empire",
+    release: 1980,
+    actors: ["Mark"],
+  },
+];
+//forEach
+movie.forEach((movie) => console.log(movie.title));
+//filter, to get a new array of movies that match criteria
+//arrayname. array command then arrow function that accepts singular elements of array
+const newMovies = movie.filter((movie) => movie.release > 1990);
+console.log(newMovies);
+ */
+const students = [
+  { name: "Alice", age: 20, grade: 85 },
+  { name: "Bob", age: 22, grade: 90 },
+  { name: "Charlie", age: 21, grade: 78 },
+  { name: "Diana", age: 23, grade: 95 },
+];
